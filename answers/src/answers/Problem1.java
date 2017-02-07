0d02bf6ddc3079e3987d905090a8890047e10aeb
siroring <whiteloli99@gmail.com>
2017-02-07 22:22:37 +0900

    problem 1 answers fix - one line 80 characters

--- answers/src/answers/Problem1.java
+++ answers/src/answers/Problem1.java
@@ -4,12 +4,14 @@ package answers;
  * Created by seok on 2017. 2. 5..
  *
  *  Problem 1
- * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5,
+ * we get 3, 5, 6 and 9. The sum of these multiples is 23.
  * Find the sum of all the multiples of 3 or 5 below 1000.
  */
 public class Problem1 {
     public static void main(String[] args) {
-        int result = sumMultiples(3, 1000) + sumMultiples(5, 1000) - sumMultiples(15, 1000);
+        int result = sumMultiples(3, 1000) + sumMultiples(5, 1000)
+                - sumMultiples(15, 1000);
         System.out.println(result);
     }
 
