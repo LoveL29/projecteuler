36b1e9d91a4b611611286bd0f0b9fd5b44b01e07
siroring <whiteloli99@gmail.com>
2017-02-11 13:33:30 +0900

    problem 7 answers

---
+++ answers/src/answers/Problem7.java
@@ -0,0 +1,34 @@
+package answers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by seok on 2017. 2. 11..
+ *
+ * Problem 7
+ *
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
+ * we can see that the 6th prime is 13.
+ * What is the 10001st prime number?
+ */
+public class Problem7 {
+    public static void main(String[] args) {
+        List<Integer> primeList = new ArrayList<>();
+        primeList.add(2);
+        int i = 3;
+        while (primeList.size() <= 10000) {
+            boolean isPrime = true;
+            for (Integer prime : primeList) {
+                if (i % prime == 0) {
+                    isPrime = false;
+                }
+            }
+            if (isPrime) {
+                primeList.add(i);
+            }
+            i++;
+        }
+        System.out.println(primeList.get(10000));
+    }
+}
