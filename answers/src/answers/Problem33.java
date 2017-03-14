32b83a93ce2f3162278234281b2f313dc3571c5d
Siroring <whiteloli99@gmail.com>
2017-03-14 21:21:45 +0900

    problem 33 fix

--- answers/src/answers/Problem33.java
+++ answers/src/answers/Problem33.java
@@ -41,11 +41,9 @@ public class Problem33 {
     }
 
     private static int gcd(int a, int b) {
-        while (b != 0) {
-            int tmp = a % b;
-            a = b;
-            b = tmp;
+        if (a == 0 || b == 0) {
+            return a + b;
         }
-        return Math.abs(a);
+        return gcd(b,a%b);
     }
 }
